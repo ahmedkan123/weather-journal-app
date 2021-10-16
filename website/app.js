c159e90abc4dc22f59c0fece843f8d35d657f46e
ahmed <ahmedabdelgawad951@gmail.com>
2021-10-16 11:00:03 +0200

    update changes udacity on the project

--- website/app.js
+++ website/app.js
@@ -14,17 +14,30 @@ generateButton.addEventListener("click", async () => {
       alert("please you should enter zipcode");
       return;
     }
-    const fulUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
-    const response = await fetch(fulUrl);
-    const dataWeather = await response.json();
-    const temp = dataWeather.main.temp;
-    postDataOnServer(temp, content);
-    getDataFromServer();
+    getDataWeatherTemp(zipCode)
+      .then((temp) => {
+        postDataOnServer(temp, content);
+      })
+      .then(getDataFromServer)
+      .then((representData) => {
+        updateUiData(representData);
+      });
+
+    // postDataOnServer(temp, content);
+    // getDataFromServer();
   } catch (error) {
     console.log("Error:", error);
   }
 });
 
+// function get data from external api and return temp
+async function getDataWeatherTemp(zipCode) {
+  const fulUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+  const response = await fetch(fulUrl);
+  const dataWeather = await response.json();
+  const temp = dataWeather.main.temp;
+  return temp;
+}
 // function to save to data on server by endpoint post method
 async function postDataOnServer(temp, content) {
   await fetch("/saveDataWeather", {
@@ -45,6 +58,18 @@ async function getDataFromServer() {
   const getDataResponse = await fetch("/getWeatherData");
   const representData = await getDataResponse.json();
   // console.log(representData);
+  //// update ui dynamically
+  // const dateDay = document.querySelector("#date");
+  // const tempCountry = document.querySelector("#temp");
+  // const contentFeelings = document.querySelector("#content");
+  // dateDay.innerHTML = `Date is : ${representData.date}`;
+  // tempCountry.innerHTML = `Temp is : ${representData.temp}`;
+  // contentFeelings.innerHTML = `Content is : ${representData.content}`;
+  return representData;
+}
+
+// function for update ui to data dynamically
+async function updateUiData(representData) {
   const dateDay = document.querySelector("#date");
   const tempCountry = document.querySelector("#temp");
   const contentFeelings = document.querySelector("#content");
